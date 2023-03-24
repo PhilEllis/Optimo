18e08d3a955c6bec00f41470813dd382bf8e5f70
PhilEllis <philippamacnab@hotmail.com>
2023-03-24 22:48:24 +0000

    aos added to the homepage

--- assets/js/main.js
+++ assets/js/main.js
@@ -242,11 +242,13 @@
 
   });
 
-  /**
-   * Initiate Pure Counter
+    /**
+   * Initiate portfolio lightbox 
    */
-  new PureCounter();
-
+    const portfolioLightbox = GLightbox({
+      selector: '.portfolio-lightbox'
+    });
+  
   /**
    * Portfolio details slider
    */
@@ -263,5 +265,22 @@
       clickable: true
     }
   });
+
+  /**
+   * Animation on scroll
+   */
+  window.addEventListener('load', () => {
+    AOS.init({
+      duration: 1000,
+      easing: 'ease-in-out',
+      once: true,
+      mirror: false
+    })
+  });
+  
+  /**
+   * Initiate Pure Counter
+   */
+ new PureCounter();
 
 })()
