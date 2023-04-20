116cb955a2c72f321d0b6b6e7f4e29fb426480a6
PhilEllis <philippamacnab@hotmail.com>
2023-04-20 13:55:08 +0000

    error command in contcat form

--- forms/contact.php
+++ forms/contact.php
@@ -26,7 +26,7 @@
   // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
   
   $contact->smtp = array(
-    'host' => 'gmail.com',
+    'host' => 'smtp.gmail.com',
     'username' => 'optimocontactform@gmail.com',
     'password' => 'Optimo2023!',
     'port' => '587'
@@ -36,6 +36,6 @@
   $contact->add_message( $_POST['name'], 'From');
   $contact->add_message( $_POST['email'], 'Email');
   $contact->add_message( $_POST['message'], 'Message', 10);
-
+  $contact->option( 'SMTPDebug', 3);
   echo $contact->send();
 ?>
