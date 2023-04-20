8e5a81f6868223d7e6c4c8fc148f5cec383e2a20
PhilEllis <philippamacnab@hotmail.com>
2023-04-20 11:27:16 +0000

    removal of smtp from gmail.com

--- forms/contact.php
+++ forms/contact.php
@@ -26,11 +26,10 @@
   // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
   
   $contact->smtp = array(
-    'host' => 'smtp.gmail.com',
+    'host' => 'gmail.com',
     'username' => 'optimocontactform@gmail.com',
     'password' => 'Optimo2023!',
     'port' => '587'
-    'encryption' => 'tls'
   );
   
 
